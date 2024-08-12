83a82e28c545fcb3bcdeaa2fdaef185faf16a31b
C00330 <akmal.mahathir@tmrnd.com.my>
2024-08-12 21:37:28 +0800

    Homepage Navigation

--- test/pageobjects/homepage.page.ts
+++ test/pageobjects/homepage.page.ts
@@ -4,7 +4,12 @@ class HomePage {
 	//Element Selectors
 	searchIcon = 'android=new UiSelector().className("com.horcrux.svg.PathView").instance(1)';
 	searchField = "android.widget.EditText";
-	abbrevPI ='android=new UiSelector().text("Abbrev PI")'
+	abbrevPI = 'android=new UiSelector().text("Abbrev PI")';
+	navHomeIcon = 'android=new UiSelector().text("Home")';
+	navSearchIcon = 'android=new UiSelector().text("Search")';
+	navInteractionIcon = 'android=new UiSelector().description(", Interactions")';
+	navPillIdIcon = 'android=new UiSelector().description(", Pill Identifier")';
+	navResourcesIcon = 'android= new UiSelector().description(", Resources")';
 
 	//Search Function for Homepage
 	async clickSearchIcon() {
@@ -28,13 +33,13 @@ class HomePage {
 		const clickDrugDetails = await $(selector);
 		let isSummaryPresent = false;
 		// const drugNameSelector = `android=new UiSelector().text("${drugName}")`;
-		const drugNameSelector = this.abbrevPI;
+		const abbrevPIchecker = this.abbrevPI;
 
-        //This Do-While loop is necessary because the app won't register the click intermitently
+		//This Do-While loop is necessary because the app won't register the click intermitently
 		do {
 			await clickDrugDetails.click();
 			await browser.pause(1000); // Optional: Add a short pause to avoid too rapid clicking
-			isSummaryPresent = await $(drugNameSelector).isExisting();
+			isSummaryPresent = await $(abbrevPIchecker).isExisting();
 		} while (!isSummaryPresent);
 		console.log(`summary is now present`);
 	}
@@ -44,6 +49,48 @@ class HomePage {
 	//Browse Active
 
 	//Drug Update
+
+	//Home Tab
+	async clickHomeTab() {
+		const navHomeIcon = await $(this.navHomeIcon);
+		const isDisplayed = await navHomeIcon.waitForDisplayed({ timeout: 5000 });
+		expect(isDisplayed).toBe(true);
+		await navHomeIcon.click();
+		return navHomeIcon;
+	}
+
+	//Search Tab
+	async clickSearchTab() {
+		const navSearchIcon = await $(this.navSearchIcon);
+		const isDisplayed = await navSearchIcon.waitForDisplayed({ timeout: 5000 });
+		expect(isDisplayed).toBe(true);
+		// await navSearchIcon.click();
+		return navSearchIcon;
+	}
+
+	//Interactions Tab
+	async clickInteractionTab() {
+		const navInteractionIcon = await $(this.navInteractionIcon);
+		const isDisplayed = await navInteractionIcon.waitForDisplayed({ timeout: 5000 });
+		expect(isDisplayed).toBe(true);
+		await navInteractionIcon.click();
+	}
+
+	//Pill Identifier Tab
+	async clickPillIdTab(){
+		const navPillIdIcon = await $(this.navPillIdIcon);
+		const isDisplayed = await navPillIdIcon.waitForDisplayed({ timeout: 5000 });
+		expect(isDisplayed).toBe(true);
+		await navPillIdIcon.click();
+	}
+
+	//Resources Tab
+	async clickResourcesTab(){
+		const navResourcesIcon = await $(this.navResourcesIcon);
+		const isDisplayed = await navResourcesIcon.waitForDisplayed({ timeout: 5000 });
+		expect(isDisplayed).toBe(true);
+		await navResourcesIcon.click();
+	}
 }
 
 export default new HomePage();
--- test/specs/homepage.spec.ts
+++ test/specs/homepage.spec.ts
@@ -8,6 +8,8 @@ dotenv.config();
 describe("Homepage functionality test", () => {
 	// Assigning locators to varaibles
 	let searchField: WebdriverIO.Element;
+	let testNavIcon: WebdriverIO.Element;
+	let testHomeIcon: WebdriverIO.Element;
 	let baseUrl: any;
 
 	before(async () => {
@@ -16,6 +18,7 @@ describe("Homepage functionality test", () => {
 		console.log(baseUrl);
 	});
 
+	//Can be ignored later, this is for PoC for GET request to get token
 	it('should be able to get the request',async()=>{
 		try {
 			const result = await sendAuthRequest(baseUrl);
@@ -25,21 +28,54 @@ describe("Homepage functionality test", () => {
 		  }
 	});
 
+	//TC1: Navigation Bar: Search
+	it('should check if navigation bar: Search is displayed and functional',async()=>{
+		testNavIcon = await HomePage.clickSearchTab();
+		testNavIcon.click();
+	});
+
+	//TC2: Navigation Bar: Interactions
+	it('should check if navigation bar: Interactions is displayed and functional',async()=>{
+		HomePage.clickInteractionTab();
+	});
+
+	//TC3: Navigation Bar: Pill Identifier
+	it('should check if navigation bar: Pill Identifier is displayed and functional',async()=>{
+		HomePage.clickPillIdTab();
+	});
+
+	//TC4: Navigation Bar: Resources
+	it('should check if navigation bar: Resources is displayed and functional',async()=>{
+		HomePage.clickResourcesTab();
+	});
+
+	//TC5: Navigation Bar: Home
+	it('should check if navigation bar: Home is displayed and functional',async()=>{
+		//For some reason the element need to be clicked twice
+		testHomeIcon = await HomePage.clickHomeTab();
+		HomePage.clickHomeTab();
+	})
+	
+	//TC6: Home: Search Icon is Clickable
 	it("should check if search icon is displayed and functional", async () => {
 		HomePage.clickSearchIcon();
 	});
 
+	//TC7: Home: Search Field is Present
 	it("should check if search field is displayed", async () => {
 		const isDisplayed = await searchField.waitForDisplayed({ timeout: 5000 });
 		expect(isDisplayed).toBe(true);
 	});
 
+	//TC8: Home: Search Drug
 	it("should be able to input value to the search field", async () => {
+		//Getting random Drug Names from the list
 		const drugNamesArray = Object.values(drugNames);
 		const randomIndex = Math.floor(Math.random() * drugNamesArray.length);
 		const randomDrugName = drugNamesArray[randomIndex];
 		const randomDrugSelector = drugNamesSelector[randomDrugName];
 		
+		//Start of test Case
 		await HomePage.setSearchField(randomDrugName);
 		const drugElement = await $(randomDrugSelector);
 		await drugElement.waitForDisplayed({ timeout: 5000 });
